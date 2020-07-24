af68c388119a21c76cbe724008c5ec382aef0288
Kostiantyn Zadyraka <zadyraka.k@gmail.com>
2020-07-24 21:25:39 +0300

    Now can choose files from other directories, browse through other directories, skip bad files and show ErrorScreen if bad file chosen

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from PIL import Image, ImageTk
 from tkinter import Tk, Label, Button
 import os
+from tkinter import filedialog, messagebox
+
+
+def choose_pic():
+    global current_image
+    place_image.grid_forget()
+    filename = filedialog.askopenfilename(initialdir='D:\кек\Cool',
+                                          title='Choose file',
+                                          filetypes=(('all files', '*.*'), ('png files', '*.png'), ('JPEG files', '*.jpg'))
+                                          )
+    try:
+        current_image = Image.open(filename)
+        create_pic()
+        change_dir(filename)
+    except OSError:
+        messagebox.showerror("Error", "Unsupported file type")
+        choose_pic()
+
+
+def change_dir(path):
+    global myfiles, directory
+    directory = os.path.dirname(path)
+    myfiles = os.listdir(directory)
+    print(directory)
+    print(myfiles)
 
 
 def resize_image():
@@ -20,31 +45,39 @@ def create_pic():
     resize_image()
     a = ImageTk.PhotoImage(current_image)
     place_image = Label(image=a, width=current_image.width, height=current_image.height)
-    place_image.grid(row=1, column=0, columnspan=2)
+    place_image.grid(row=1, column=0, columnspan=3)
 
 
 def next_pic():
     global current_image
     global count
     global place_image
+    global directory
     place_image.grid_forget()
     count += 1
     if count == len(myfiles):
         count = 0
-    current_image = Image.open('img/{x}'.format(x=myfiles[count]))
-    create_pic()
+    try:
+        current_image = Image.open('{dir}/{x}'.format(dir=directory, x=myfiles[count]))
+        create_pic()
+    except OSError:
+        next_pic()
 
 
 def previous_pic():
     global current_image
     global count
     global place_image
+    global directory
     place_image.grid_forget()
     count -= 1
     if count == -1:
         count = len(myfiles) - 1
-    current_image = Image.open('img/{x}'.format(x=myfiles[count]))
-    create_pic()
+    try:
+        current_image = Image.open('{dir}/{x}'.format(dir=directory, x=myfiles[count]))
+        create_pic()
+    except OSError:
+        previous_pic()
 
 
 root = Tk()
@@ -52,15 +85,18 @@ root.title('Image Viewer')
 root.iconbitmap('icon.ico')
 
 myfiles = os.listdir('img')
+directory = 'img'
 
 count = 0
 current_image = Image.open('img/{x}'.format(x=myfiles[count]))
 
 forward_button = Button(text='Forward', command=next_pic)
 back_button = Button(text='Back', command=previous_pic)
+choose_button = Button(text='Open picture', command=choose_pic)
 
-forward_button.grid(row=0, column=1)
+forward_button.grid(row=0, column=2)
 back_button.grid(row=0, column=0)
+choose_button.grid(row=0, column=1)
 
 create_pic()
 
